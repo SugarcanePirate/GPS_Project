da0ed4cfe5bf9e809dbf7901e2d85481a97de62f
DanielRosa777 <danyel.rosa777@gmail.com>
2016-11-25 03:29:38 +0000

    Class ingrediente

---
+++ src/LogicPackage/Ingrediente.java
@@ -0,0 +1,11 @@
+package LogicPackage;
+
+import java.io.Serializable;
+
+public abstract class Ingrediente implements Serializable {
+
+    private String nome;
+    private int calorias;
+    private int quantidade;
+
+}
