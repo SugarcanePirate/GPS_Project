8a091332440df917b887b690d91a848a6d6e11f5
DanielRosa777 <danyel.rosa777@gmail.com>
2016-11-25 03:51:38 +0000

    Class ingrediente

---
+++ src/LogicPackage/Vegetais.java
@@ -0,0 +1,12 @@
+package LogicPackage;
+
+/*
+*Classe Vegetais extende a classe abstracta Ingrediente
+ */
+public class Vegetais extends Ingrediente {
+
+    public Vegetais(String nome, int calorias, int quantidade) {
+        super(nome, calorias, quantidade);
+    }
+
+}
